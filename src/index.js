25dba0991fb96c632a4e02bea932699f4628f4fc
kosandrey <36664836+kosandrey@users.noreply.github.com>
2018-02-28 11:44:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,88 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var data;
+  var brackets = [];
+  var correct  = false;
+  var i;
+  for(i = 0; i<str.size; i++)
+  {
+    data = str[i];
+    if(data == '(' || data == '[' || data == '{' || data == '1' || data == '5' || data == '3')
+    {
+      brackets.push(data);
+      correct = false;
+      continue;
+    }
+    else if(data == ')' || data == ']' || data == '}' || data == '2' || data == '6' || data == '4')
+    {
+      if(data == ')' && brackets[brackets.size -1] == '(')
+      {
+        brackets.pop();
+        correct = true;
+      }
+      else if(data == ']' && brackets[brackets.size -1] == '[')
+      {
+        brackets.pop();
+        correct = true;
+      }
+      else if(data == '}' && brackets[brackets.size -1] == '{')
+      {
+        brackets.pop();
+        correct = true;
+      }
+      else if(data == '2' && brackets[brackets.size -1] == '1')
+      {
+        brackets.pop();
+        correct = true;
+      }
+      else if(data == '4' && brackets[brackets.size -1] == '3')
+      {
+        brackets.pop();
+        correct = true;
+      }
+      else if(data == '6' && brackets[brackets.size -1] == '5')
+      {
+        brackets.pop();
+        correct = true;
+      }
+      if(correct == true) continue;
+      else break;
+    }
+    else if(data == '|' || data == '7' || data =='8')
+    {
+      if(data == '|'  && [brackets.size -1] == '|')
+      {
+        brackets.pop()
+        correct = true
+      }
+      else if (data == '|'  && [brackets.size -1] != '|')
+      {
+          brackets.push(data);
+          correct = false;
+      }
+      else if (data == '7'  && [brackets.size -1] == '7')
+      {
+          brackets.push(data);
+          correct = true;
+      }
+      else if (data == '7'  && [brackets.size -1] != '7')
+      {
+          brackets.push(data);
+          correct = false;
+      }
+      else if (data == '8'  && [brackets.size -1] == '8')
+      {
+          brackets.push(data);
+          correct = true;
+      }
+      else if (data == '8'  && [brackets.size -1] != '8')
+      {
+          brackets.push(data);
+          correct = false;
+      }
+    }
+  }
+  //correct = true;
+  if (correct == true) return true;// your solution
+  else return true;
 }
+
